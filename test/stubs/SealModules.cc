6a163120744068d0627d8e55912c405f12fac15d
W.David Dagenhart <sha1-9ca965af9cf1904746014c29a94c3544e5eaff4b@cern.ch>
2007-02-14 20:45:13 +0000

    Modify service infrastructure so that service class constructors, destructors, and service callbacks like postBeginJob will get called in specified order.  First, default services, then forced services, then services from the configuration.  There are also some services explicitly created in the code that fit in where they are made.  Pre type callbacks are called in the same order as the constructors.  Post type callbacks and destructors are called in reverse order of construction.  New related unit test.

--- test/stubs/SealModules.cc
+++ test/stubs/SealModules.cc
@@ -8,6 +8,7 @@
 
 #include "FWCore/ServiceRegistry/test/stubs/DependsOnDummyService.h"
 #include "FWCore/ServiceRegistry/test/stubs/DummyService.h"
+#include "FWCore/ServiceRegistry/test/stubs/DummyServiceE0.h"
 #include "FWCore/ServiceRegistry/interface/ServiceMaker.h"
 
 using namespace testserviceregistry;
@@ -15,4 +16,8 @@ using namespace edm::serviceregistry;
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_SERVICE_MAKER(DependsOnDummyService,NoArgsMaker<DependsOnDummyService>);
 DEFINE_ANOTHER_FWK_SERVICE(DummyService);
-
+DEFINE_ANOTHER_FWK_SERVICE(DummyServiceE0);
+DEFINE_ANOTHER_FWK_SERVICE(DummyServiceA1);
+DEFINE_ANOTHER_FWK_SERVICE(DummyServiceD2);
+DEFINE_ANOTHER_FWK_SERVICE(DummyServiceB3);
+DEFINE_ANOTHER_FWK_SERVICE(DummyServiceC4);
